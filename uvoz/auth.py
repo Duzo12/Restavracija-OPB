db996af5a4576c397c7abc6b2711cde64e29377b
jankoprogramko <31898545+jankoprogramko@users.noreply.github.com>
2021-05-13 11:36:02 +0200

    popravljen gitignore

--- uvoz/auth.py
+++ uvoz/auth.py
@@ -1,5 +1,5 @@
 # Skopiraj datoteko v auth.py in vnesi podatke za priklop na bazo
 db = 'sem2021_nejcd'
 host = 'baza.fmf.uni-lj.si'
-user = 'nejcd'
-password = 'Studentm26'
+user = 'janc'
+password = 'ztqa18e4'
